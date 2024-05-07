d7dd4bfb9968a0fd8c47ef41a7c68093742defa8
Muhammad Aditya Nugraha <137638416+adityanugraha1103@users.noreply.github.com>
2024-05-07 09:32:14 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+    echo "<h1>Perkenalkan</h1>";
+    echo "Nama saya Muhammad Aditya Nugraha
+    Saya tinggal di Pondok Ranji
+    Hobi saya adalah bermain game dan Menonton Film";
+}
+}
